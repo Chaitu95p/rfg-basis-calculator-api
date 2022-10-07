9c7e6112075ade3137aa58fd1a818e72eb6343ab
Chaitanya A S <aschaitanya519@gmail.com>
2022-10-07 21:24:43 +0530

    Adding SpringBoot Project

---
+++ src/main/java/com/socgen/rfg/RfgBasisCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.socgen.rfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfgBasisCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfgBasisCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socgen/rfg/RfgBasisCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socgen.rfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfgBasisCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
