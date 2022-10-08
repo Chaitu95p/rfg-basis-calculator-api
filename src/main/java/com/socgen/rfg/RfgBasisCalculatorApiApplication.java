d376499db934764797586e7eb2814f9a07e8d7eb
Chaitanya A S <aschaitanya519@gmail.com>
2022-10-08 09:19:54 +0530

    Updated the main class by adding welcome endpoint

--- src/main/java/com/socgen/rfg/RfgBasisCalculatorApiApplication.java
+++ src/main/java/com/socgen/rfg/RfgBasisCalculatorApiApplication.java
@@ -2,9 +2,17 @@ package com.socgen.rfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RfgBasisCalculatorApiApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to the Maven World..!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RfgBasisCalculatorApiApplication.class, args);
